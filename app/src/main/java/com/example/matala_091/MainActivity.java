8482464f67484212a87bb19d1c0741b3da568b47
yp6497 <59597619+yp6497@users.noreply.github.com>
2020-10-16 16:43:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/matala_091/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.matala_091;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String st=item.getTitle().toString();
+        if(st.endsWith("Blue"))
+            getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+        if(st.endsWith("Red"))
+            getWindow().getDecorView().setBackgroundColor(Color.RED);
+        if(st.endsWith("Yellow"))
+            getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+
+        return true;
+    }
+
+
+    public void moveP(View view) {
+        //getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+        Intent si = new Intent(this,SecondActivity.class);
+        startActivity(si);
+    }
+}
---
+++ app/src/main/java/com/example/matala_091/SecondActivity.java
@@ -0,0 +1,50 @@
+package com.example.matala_091;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+        menu.add("Black");
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        String st=item.getTitle().toString();
+
+        if(st.endsWith("Blue"))
+            getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+        if(st.endsWith("Red"))
+            getWindow().getDecorView().setBackgroundColor(Color.RED);
+        if(st.endsWith("Yellow"))
+            getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+        if(st.endsWith("Black"))
+            getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+
+        return true;
+    }
+
+    public void returnB(View view) {
+        finish();
+    }
+}
